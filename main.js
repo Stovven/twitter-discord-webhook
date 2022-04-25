5579d41cf434b3bb57da72f41845fdbc84ff1e8f
stovven <29066962+Stovven@users.noreply.github.com>
2022-04-24 23:48:38 -0400

    fixes to only support tweets, and for uptime

--- main.js
+++ main.js
@@ -24,8 +24,24 @@ tClient.get('account/verify_credentials', { skip_status: true })
 var stream = tClient.stream('statuses/filter', { follow: `${config.user_id}` })
 console.log(`Fetching tweets from id: ${config.user_id}`)
 
+stream.on('connected', function (response) {
+  console.log(`Connected to twitter`)
+})
+
 stream.on('tweet', function (tweet) {
-    if(tweet.retweeted_status) return
+  if(tweet.retweeted_status || tweet.in_reply_to_status_id || tweet.is_quote_status) return
     let twitterLink = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
     wClient.send(`${config.webhook_message} ${twitterLink}`)
   })
+  
+stream.on('disconnect', function (disconnectMessage) {
+  console.log(`Disconnected from twitter`)
+})
+
+stream.on('reconnect', function (request, response, connectInterval) {
+    console.log(`reconnecting to twitter`)
+})
+
+process.on('uncaughtException', function (err) {
+      console.log("something happened!", err);
+});
